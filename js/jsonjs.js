00b4f7e553455681c5c837b8188c8f5e6a650f4b
lokosoff98 <52084682+lokosoff98@users.noreply.github.com>
2019-06-22 00:02:11 +0500

    Add files via upload

---
+++ js/jsonjs.js
@@ -0,0 +1,8 @@
+"use strict";
+
+(function() {
+	var jsonNumber = [34, 578, 888];
+	var jsonNumberSrt = JSON.stringify(jsonNumber);
+
+	window.jsonData(jsonNumberSrt);
+})();
---
+++ js/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+(function() {
+
+	// var cloneElem = function(dataClone) {
+	// 	var elemApp = document.querySelector("#template").content.querySelector(".elem").cloneNode(true);
+	// 		elemApp.children[0].textContent = dataClone;
+	// 		document.body.appendChild(elemApp);
+	// }
+
+	// window.jsonData = function(dataNumber) {
+	// 	var dataPars = JSON.parse(dataNumber);
+	// 	for (var i = 0; i < dataPars.length; i++){
+	// 		cloneElem(dataPars[i]);
+	// 	}
+	// };
+
+	// var loader = document.createElement("script");
+	// loader.src = "js/script-callback.js";
+	// document.body.append(loader);
+
+	var xhr = new XMLHttpRequest();
+
+	console.log(xhr);
+})();
+
